f66a953c214fe1f98243c3c2960f86ae94688e09
Andrea Niklaus <andreaniklaus@hotmail.com>
2024-03-19 15:54:26 +0100

    Login-File added

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Header from './components/Header';
 import LearningGoals from './components/LearningGoals';
 import ExamDates from './components/ExamDates';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <main>
+        <Login />
         <LearningGoals />
         <ExamDates />
       </main>
